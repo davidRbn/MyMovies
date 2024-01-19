c4cf49718566c9009351c22aeaf3b6e0c3e4248d
ruben <davidruben14@gmail.com>
2024-01-19 12:53:31 +0100

    add error text login and signup

--- src/components/auth/signin/Signin.jsx
+++ src/components/auth/signin/Signin.jsx
@@ -1,97 +1,145 @@
-import { Button, Input, InputLabel} from '@material-ui/core'
-import SendIcon from '@material-ui/icons/Send';
-import React, { useContext } from 'react'
-import { useState } from 'react';
-import { useHistory } from 'react-router';
-import apiUser from '../../conf/api.users';
-import { AuthContext } from '../../context/contextAuth';
-import styles from '../../styles/styles';
-import Loader from '../../loader/Loader'
-import ModalSignin from './modalSignin/ModalSignin';
+import { Button, Input, InputLabel } from "@material-ui/core";
+import SendIcon from "@material-ui/icons/Send";
+import React, { useContext } from "react";
+import { useState } from "react";
+// import { useHistory } from "react-router";
+import apiUser from "../../conf/api.users";
+import { AuthContext } from "../../context/contextAuth";
+import styles from "../../styles/styles";
+import Loader from "../../loader/Loader";
+import ModalSignin from "./modalSignin/ModalSignin";
 
-const Signin = ({data,handleInputChange,setActionAuth}) => {
-  
-    const history = useHistory()
-    // eslint-disable-next-line no-unused-vars
-    const [state,dispatch] = useContext(AuthContext)
-    const [loaderSignin,setLoaderSignin] = useState(false)
-    const [signinModal,setsigninModal] = useState(true)
-
+const Signin = ({ data, handleInputChange, setActionAuth }) => {
+  //   const history = useHistory();
+  // eslint-disable-next-line no-unused-vars
+  const [state, dispatch] = useContext(AuthContext);
+  const [loaderSignin, setLoaderSignin] = useState(false);
+  const [signinModal, setsigninModal] = useState(true);
+  const [error, setError] = useState(null);
 
   const styleLogin = {
-    inputLabel:{
-        marginTop:'20px',
-        color: styles.secondaryColor
+    inputLabel: {
+      marginTop: "20px",
+      color: styles.secondaryColor,
     },
-    input:{
-        borderBottom: `1px solid ${styles.secondaryColor}`,
-        color:styles.secondaryColor
+    input: {
+      borderBottom: `1px solid ${styles.secondaryColor}`,
+      color: styles.secondaryColor,
     },
-    buttonSend:{
-        height:'30px',
-        width:'200px',
-        marginTop:'20px',
-        padding:'0',
-        color: styles.thirdColor,
-        backgroundColor:styles.primaryColor
+    buttonSend: {
+      height: "30px",
+      width: "200px",
+      marginTop: "20px",
+      padding: "0",
+      color: styles.thirdColor,
+      backgroundColor: styles.primaryColor,
     },
-    sendIcon:{
-        paddingLeft:'10px'
+    sendIcon: {
+      paddingLeft: "10px",
     },
-    buttonSignin:{
-        cursor:'pointer',
-        color:styles.secondaryColor    
-    }
-}
+    buttonSignin: {
+      cursor: "pointer",
+      color: styles.secondaryColor,
+    },
+  };
 
-const signinUser = async (e) => {
-    e.preventDefault()
-    setLoaderSignin(true)
-   await apiUser.post('/users/login',{
+  const signinUser = async (e) => {
+    e.preventDefault();
+    // setLoaderSignin(true);
+    await apiUser
+      .post("/users/login", {
         email: data.email,
-        password: data.password    
-    })
-    .then(res => {
+        password: data.password,
+      })
+      .then((res) => {
         console.log(res.data);
-        setLoaderSignin(false)
+        setLoaderSignin(false);
         dispatch({
-        type: 'LOGIN',
-        payload :{
-            token:res.data.token,
-            user: res.data.userId          
-        } 
-    })})
-    .catch(err => console.log(err))
-    history.push('/')
-}
+          type: "LOGIN",
+          payload: {
+            token: res.data.token,
+            user: res.data.userId,
+          },
+        });
+      })
+      .catch((err) => {
+        console.log(err);
 
+        if (err.response) {
+          setError(err.response.data.error);
+        } else {
+          setError(err.message);
+        }
+      });
+  };
 
-    return(
-        <>
-                 <div style={{height:'80%',display:'flex',justifyContent: 'center',alignItems:'center'}}>
-        {loaderSignin? <Loader/> : <div style={{width:'200px',height:'40%',textAlign:'center'}} >
-            <div> 
-             <h2 style={{color:styles.secondaryColor}} >Se connecter</h2>
-             </div>
-        <form onSubmit={signinUser} style={{display:'flex',flexDirection:'column'}}>
-          <InputLabel style={styleLogin.inputLabel}  htmlFor='email'  >Email</InputLabel>
-            <Input style={styleLogin.input}   type='email' name='email' onChange={e => handleInputChange(e)}/>
-            <InputLabel style={styleLogin.inputLabel} htmlFor='password'>Mot de passe</InputLabel>
-            <Input style={styleLogin.input}  type='password' name='password' onChange={e => handleInputChange(e)}/>
-            <Button
-                  type='submit'
-                  style={styleLogin.buttonSend}
-                  variant="contained"
-                  color="primary"
-                     > Envoyer <SendIcon style={styleLogin.sendIcon}/>
-            </Button>
-        </form>
-        <p style={styleLogin.buttonSignin} onClick={() => setActionAuth('SIGNUP')}>S'inscrire</p>
-        </div>}
-        </div>
-        {<ModalSignin signinModal={signinModal} setsigninModal={setsigninModal}/>}
-        </>
-    )
-}
+  return (
+    <>
+      <div
+        style={{
+          height: "80%",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        {loaderSignin ? (
+          <Loader />
+        ) : (
+          <div style={{ width: "200px", height: "40%", textAlign: "center" }}>
+            <div>
+              <h2 style={{ color: styles.secondaryColor }}>Se connecter</h2>
+            </div>
+            <form
+              onSubmit={signinUser}
+              style={{ display: "flex", flexDirection: "column" }}
+            >
+              <InputLabel style={styleLogin.inputLabel} htmlFor="email">
+                Email
+              </InputLabel>
+              <Input
+                style={styleLogin.input}
+                type="email"
+                name="email"
+                onChange={(e) => handleInputChange(e)}
+              />
+              <InputLabel style={styleLogin.inputLabel} htmlFor="password">
+                Mot de passe
+              </InputLabel>
+              <Input
+                style={styleLogin.input}
+                type="password"
+                name="password"
+                onChange={(e) => handleInputChange(e)}
+              />
+              {error && <p style={{ color: "red" }}>{error}</p>}
+              <Button
+                type="submit"
+                style={styleLogin.buttonSend}
+                variant="contained"
+                color="primary"
+              >
+                {" "}
+                Envoyer <SendIcon style={styleLogin.sendIcon} />
+              </Button>
+            </form>
+            <p
+              style={styleLogin.buttonSignin}
+              onClick={() => setActionAuth("SIGNUP")}
+            >
+              S'inscrire
+            </p>
+          </div>
+        )}
+      </div>
+      {
+        <ModalSignin
+          signinModal={signinModal}
+          setsigninModal={setsigninModal}
+        />
+      }
+    </>
+  );
+};
 
-export default Signin
+export default Signin;
--- src/components/auth/signup/Signup.jsx
+++ src/components/auth/signup/Signup.jsx
@@ -1,94 +1,142 @@
-import { Button, Input, InputLabel } from '@material-ui/core'
-import SendIcon from '@material-ui/icons/Send';
-import React, { useState } from 'react'
-import apiUser from '../../conf/api.users';
-import Loader from '../../loader/Loader';
-import styles from '../../styles/styles'
+import { Button, Input, InputLabel } from "@material-ui/core";
+import SendIcon from "@material-ui/icons/Send";
+import React, { useState } from "react";
+import apiUser from "../../conf/api.users";
+import Loader from "../../loader/Loader";
+import styles from "../../styles/styles";
 
-const Signup = ({data,setActionAuth,handleInputChange}) => {
-
-    const [loaderSignup,setLoaderSignup] = useState(false)
-
+const Signup = ({ data, setActionAuth, handleInputChange }) => {
+  const [loaderSignup, setLoaderSignup] = useState(false);
+  const [error, setError] = useState(null);
 
   const styleLogin = {
-      inputLabel:{
-          marginTop:'20px',
-          color: styles.secondaryColor
-
-      },
-      input:{
-        borderBottom: `1px solid ${styles.secondaryColor}`,
-        color:styles.secondaryColor
-
+    inputLabel: {
+      marginTop: "20px",
+      color: styles.secondaryColor,
     },
-      buttonSend:{
-          height:'30px',
-          width:'200px',
-          marginTop:'20px',
-          color: styles.thirdColor,
-          backgroundColor:styles.primaryColor
-      },
-      sendIcon:{
-          paddingLeft:'10px',
-      },
-      buttonSignin:{
-          cursor:'pointer',
-          color:styles.secondaryColor
-      }
-  }
+    input: {
+      borderBottom: `1px solid ${styles.secondaryColor}`,
+      color: styles.secondaryColor,
+    },
+    buttonSend: {
+      height: "30px",
+      width: "200px",
+      marginTop: "20px",
+      color: styles.thirdColor,
+      backgroundColor: styles.primaryColor,
+    },
+    sendIcon: {
+      paddingLeft: "10px",
+    },
+    buttonSignin: {
+      cursor: "pointer",
+      color: styles.secondaryColor,
+    },
+  };
 
-  
-const registerUser = async (e) => {
-    e.preventDefault()
-    setLoaderSignup(true)
-   await apiUser.post('/users/register',{
-        email:data.email,
-        username:data.name,
-        password:data.password
-    })
-    .then(res => {
-        setLoaderSignup(false)
-        console.log('register')
-        console.log(res)
-    })
-    .catch(err=> {
-        console.log(err)
-    
-        console.error(err.message)
-        console.error(err.request)
-        console.error(err.response.status)
+  const registerUser = async (e) => {
+    e.preventDefault();
+    await apiUser
+      .post("/users/register", {
+        email: data.email,
+        username: data.name,
+        password: data.password,
+      })
+      .then((res) => {
+        setLoaderSignup(false);
+        console.log("register");
+        console.log(res);
+      })
+      .catch((err) => {
+        if (err.response) {
+          setError(err.response.data.error);
+        } else {
+          setError(err.message);
+        }
+        // console.log(err);
+        // console.error(err.message);
+        // console.error(err.response);
+        // console.error(err.response.status);
+      });
+    // setActionAuth("SIGNIN");
+  };
 
-    
-    
-    })
-    setActionAuth('SIGNIN')
+  return (
+    <div
+      style={{
+        height: "80%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      {loaderSignup ? (
+        <Loader />
+      ) : (
+        <div
+          style={{
+            width: "200px",
+            height: "40%",
+            textAlign: "center",
+            justifyContent: "space-between",
+          }}
+        >
+          <div>
+            <h2 style={{ color: styles.secondaryColor }}>S'inscrire</h2>
+          </div>
+          <form
+            onSubmit={registerUser}
+            style={{ display: "flex", flexDirection: "column" }}
+          >
+            <InputLabel style={styleLogin.inputLabel} htmlFor="email">
+              Email
+            </InputLabel>
+            <Input
+              style={styleLogin.input}
+              type="email"
+              name="email"
+              onChange={(e) => handleInputChange(e)}
+            />
+            <InputLabel style={styleLogin.inputLabel} htmlFor="name">
+              Name
+            </InputLabel>
+            <Input
+              style={styleLogin.input}
+              type="text"
+              name="name"
+              onChange={(e) => handleInputChange(e)}
+            />
+            <InputLabel style={styleLogin.inputLabel} htmlFor="password">
+              Mot de passe
+            </InputLabel>
+            {error && <p style={{ color: "red" }}>{error}</p>}
 
-}
+            <Input
+              style={styleLogin.input}
+              type="password"
+              name="password"
+              onChange={(e) => handleInputChange(e)}
+            />
 
-    return (
-        <div style={{height:'80%',display:'flex',justifyContent: 'center',alignItems:'center'}}>
-       { loaderSignup? <Loader/> : <div style={{width:'200px',height:'40%',textAlign:'center',justifyContent:'space-between'}} >
-            <div> 
-             <h2 style={{color:styles.secondaryColor}}>S'inscrire</h2>
-             </div>
-        <form onSubmit={registerUser} style={{display:'flex',flexDirection:'column'}}>
-          <InputLabel style={styleLogin.inputLabel}  htmlFor='email'  >Email</InputLabel>
-            <Input style={styleLogin.input}  type='email' name='email' onChange={e => handleInputChange(e)}/>
-            <InputLabel style={styleLogin.inputLabel} htmlFor='name' >Name</InputLabel>
-            <Input style={styleLogin.input} type='text' name='name' onChange={e => handleInputChange(e)}/>
-            <InputLabel style={styleLogin.inputLabel} htmlFor='password'>Mot de passe</InputLabel>
-            <Input style={styleLogin.input} type='password' name='password' onChange={e => handleInputChange(e)}/>
             <Button
-                  type="submit"
-                  style={styleLogin.buttonSend}
-                  variant="contained"
-                     > Envoyer <SendIcon style={styleLogin.sendIcon}/>
+              type="submit"
+              style={styleLogin.buttonSend}
+              variant="contained"
+            >
+              {" "}
+              Envoyer <SendIcon style={styleLogin.sendIcon} />
             </Button>
-        </form>
-        <p style={styleLogin.buttonSignin} onClick={() => setActionAuth('SIGNIN')}>Se connecter</p>
-        </div>}
+          </form>
+          <p
+            style={styleLogin.buttonSignin}
+            onClick={() => setActionAuth("SIGNIN")}
+          >
+            Se connecter
+          </p>
         </div>
-    )
-}
+      )}
+    </div>
+  );
+};
 
-export default Signup
+export default Signup;
