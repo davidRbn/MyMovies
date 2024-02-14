c7e4108314e3911c073e61916ba581a98e60e832
ruben <davidruben14@gmail.com>
2024-02-14 13:36:49 +0100

    add cursor pointer logout

--- src/components/conf/api.Favoris.js
+++ src/components/conf/api.Favoris.js
@@ -1,7 +1,11 @@
 import Axios from "axios";
 
+// const apiFavoris = Axios.create({
+//   baseURL: `${process.env.REACT_APP_URL_FAVORIS}`,
+// });
+
 const apiFavoris = Axios.create({
-  baseURL: `${process.env.REACT_APP_URL_FAVORIS}`,
+  baseURL: "http://localhost:8080/api",
 });
 
 export default apiFavoris;
--- src/components/header/authDiconnect/AuthDisconnect.jsx
+++ src/components/header/authDiconnect/AuthDisconnect.jsx
@@ -1,31 +1,29 @@
-import React, { useContext } from 'react'
-import ExitToAppOutlinedIcon from '@material-ui/icons/ExitToAppOutlined';
-import { AuthContext } from '../../context/contextAuth';
-import styles from '../../styles/styles'
-import { useHistory } from 'react-router-dom';
-
-
+import React, { useContext } from "react";
+import ExitToAppOutlinedIcon from "@material-ui/icons/ExitToAppOutlined";
+import { AuthContext } from "../../context/contextAuth";
+import styles from "../../styles/styles";
+import { useHistory } from "react-router-dom";
 
 const AuthDisconnect = () => {
+  // eslint-disable-next-line no-unused-vars
+  const [state, dispatch] = useContext(AuthContext);
+  let history = useHistory();
 
+  const authDisconnection = (e) => {
+    e.preventDefault();
+    history.push("/login");
+    dispatch({ type: "LOGOUT" });
+    console.log("Vous êtes deconnecté");
+  };
 
-    // eslint-disable-next-line no-unused-vars
-    const [state,dispatch] = useContext(AuthContext)
-    let history = useHistory()
+  return (
+    <>
+      <ExitToAppOutlinedIcon
+        style={{ color: styles.secondaryColor, cursor: "pointer" }}
+        onClick={(e) => authDisconnection(e)}
+      />
+    </>
+  );
+};
 
-    const authDisconnection = (e) => {
-        e.preventDefault()
-        history.push('/login')
-        dispatch({type: "LOGOUT"})
-        console.log('Vous êtes deconnecté')
-      }
-
-    return(
-        <>
-        <ExitToAppOutlinedIcon style={{color:styles.secondaryColor}} onClick={e => authDisconnection(e)}/>
-        </>
-
-    )
-}
-
-export default AuthDisconnect
+export default AuthDisconnect;
