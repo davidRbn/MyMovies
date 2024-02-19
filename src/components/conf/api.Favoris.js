deb97eb7209d684b55c5ad4763108d8cca85f889
ruben <davidruben14@gmail.com>
2024-02-19 15:34:43 +0100

    maj

--- src/components/conf/api.Favoris.js
+++ src/components/conf/api.Favoris.js
@@ -1,11 +1,11 @@
 import Axios from "axios";
 
-// const apiFavoris = Axios.create({
-//   baseURL: `${process.env.REACT_APP_URL_FAVORIS}`,
-// });
-
 const apiFavoris = Axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: `${process.env.REACT_APP_URL_FAVORIS}`,
 });
+
+// const apiFavoris = Axios.create({
+//   baseURL: "http://localhost:8080/api",
+// });
 
 export default apiFavoris;
--- src/components/conf/api.users.js
+++ src/components/conf/api.users.js
@@ -1,11 +1,11 @@
 import Axios from "axios";
 
-// const apiUser = Axios.create({
-//   baseURL: `${process.env.REACT_APP_URL_USER}`,
-// });
-
 const apiUser = Axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: `${process.env.REACT_APP_URL_USER}`,
 });
+
+// const apiUser = Axios.create({
+//   baseURL: "http://localhost:8080/api",
+// });
 
 export default apiUser;
